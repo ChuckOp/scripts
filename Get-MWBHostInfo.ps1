b4161c88ddf06dcc0fb2943d3d53c0bfe4ed8ec5
Charles Oppermann <chuckop@malwarebytes.com>
2020-03-19 23:14:44 -0400

    Initial commit of system info collection script

---
+++ Get-MWBHostInfo.ps1
@@ -0,0 +1,42 @@
+# PowerShell script to collect information about machine
+# Written by Charles Oppermann (chuckop@malwarebytes.com) March 16, 2020
+
+# Trying to discover:
+# Exact Windows version information including SKU and release branch
+# Server roles if enabled
+# Number of user profiles
+# Version information of Malwarebytes components
+# Presence of any files in %programdata%\Malwarebytes\MBAMService\TMP
+
+# Secondary goals
+# Clean up TMP folder
+# Clean up any orphaned users
+
+
+# Windows Version, type, and SKU information
+$Win32OS = Get-WmiObject Win32_OperatingSystem
+
+$Win32OS | Select-Object Version, Caption, BuildType, NumberOfUsers, OSProductSuite, SuiteMask, OperatingSystemSKU, OSType
+
+if ($Win32OS.OSProductSuite -band 16) { "Terminal Server detected" } else { "Terminal Server not detected" }
+
+# Malwarebytes Version Information
+$MBBinaryPath = ($env:ProgramFiles+"Malwarebytes\Anti-Malware")
+Get-ChildItem -Path $MBBinaryPath\* -Include *.exe,*.dll | ForEach-Object VersionInfo
+
+# User profile information
+Get-WmiObject win32_userprofile | Select-Object SID, LocalPath, LastUseTime, Loaded
+
+Get-ChildItem ($env:HOMEDRIVE+"\Users") -hidden | Format-Table
+
+# Get Microsoft-Windows-User Profile Service/Operational Log
+Get-WinEvent "Microsoft-Windows-User Profile Service/Operational" | Format-Table -AutoSize
+
+# List out files in the service folder
+# Some modules that use OffineUAManager use TMP others don't
+$MBDataPath = ($env:ProgramData+"Malwarebytes\MBAMService")
+Get-ChildItem -Path $MBDataPath -Recurse
+
+
+
+
